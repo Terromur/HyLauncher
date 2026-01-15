611a6973e604328c41ecd5a618848412822df67b
Arch Dev <matthewjobava@gmail.com>
2026-01-15 19:36:53 +0400

    updated to 0.5.2

--- app/app.go
+++ app/app.go
@@ -69,7 +69,7 @@ func (a *App) emitError(err error) {
 	}
 }
 
-const AppVersion string = "0.5.1"
+const AppVersion string = "0.5.2"
 
 func (a *App) GetVersions() (currentVersion string, latestVersion string) {
 	current := pwr.GetLocalVersion()
