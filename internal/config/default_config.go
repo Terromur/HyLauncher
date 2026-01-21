134d2a1508f86798c3ab9ff816e68778a374ea7d
ArchDev <matthewjobava@gmail.com>
2026-01-21 12:41:20 +0400

    updated to 0.6.6

--- internal/config/default_config.go
+++ internal/config/default_config.go
@@ -2,7 +2,7 @@ package config
 
 func Default() Config {
 	return Config{
-		Version:            "0.6.5",
+		Version:            "0.6.6",
 		Nick:               "HyLauncher",
 		CurrentGameVersion: 0,
 		Branch:             "release",
